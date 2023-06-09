b6b86d0d14a0fe53246afb1f1191fc63963763dd
Addie Mongelli <addie.mongelli@stash.com>
2023-06-09 17:25:44 -0400

    have an array with working posts

--- hello.go
+++ hello.go
@@ -11,33 +11,55 @@ import (
 // embedding app.Compo into a struct.
 type hello struct {
 	app.Compo
-	text string
+	posts []blogPost
 }
 
 // The Render method is where the component appearance is defined. Here, a
 // "Hello World!" is displayed as a heading.
 func (h *hello) Render() app.UI {
-	var post = blogPost{
-		title:   "The Downfall of Neo-Noir",
-		author:  "Addie Daria",
-		content: "Pretentious drivel",
-		date:    time.Date(2023, time.May, 25, 18, 50, 00, 05, time.Local),
+	// temporarily hardcode the data!
+	h.posts = []blogPost{
+		blogPost{
+			title:   "The Downfall of Neo-Noir",
+			author:  "Addie Daria",
+			content: "Pretentious drivel",
+			date:    time.Date(2023, time.May, 25, 18, 50, 00, 05, time.Local),
+		},
+
+		blogPost{
+			title:   "Why Carrots are Problematic",
+			author:  "Addie Daria",
+			content: "AGGGGGGGGHHHHHHHHHHH",
+			date:    time.Date(2023, time.May, 31, 12, 26, 00, 00, time.Local),
+		},
 	}
 
-	var postUI = renderBlogPost(post)
-	return app.Div().Body(
-		postUI,
-	)
+	app.Logf("%d Posts: %v\n", len(h.posts), h.posts)
+
+	var postUIs = []app.UI{}
+
+	for _, post := range h.posts {
+		var postUI = renderBlogPost(post)
+		postUIs = append(postUIs, postUI)
+		app.Logf("Posts: %v\n", post)
+	}
+
+	postUIs = append(postUIs, app.Button().Text("Hello World!").OnClick(func(ctx app.Context, e app.Event) {
+
+	}))
+
+	app.Logf("%d Posts:, %v\n", len(postUIs), postUIs)
+	return app.Div().Body(postUIs...)
 
 	//return app.Div().Body(
 	//	app.Div(),
 	//	app.Textarea().Text(h.text),
-	//	app.Button().Text("Hello World!").OnClick(func(ctx app.Context, e app.Event) {
+	//	app.Button().Text(\"Hello World!").OnClick(func(ctx app.Context, e app.Event) {
 	//		h.text = "Hello World!"
 	//		h.Update()
 	//
-	//	}),
-	//)
+	//	})
+
 }
 
 type blogPost struct {
